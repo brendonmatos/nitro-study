0a44992cf1f82597044c3d7b88544f56f45000a9
brendonmatos <brendonferreiradm@gmail.com>
2023-05-06 10:45:48 -0300

    add break lines

--- routes/index.ts
+++ routes/index.ts
@@ -2,8 +2,8 @@ export default defineEventHandler(() => `<div>
   <h1>Home!</h1>
   <p>
 
-    <a href="/brendon">brendon - static</a>
-    <a href="/brendon5sec">brendon5sec - ssr with 5 sec cache age</a>
+    <a href="/brendon">brendon - static</a><br />
+    <a href="/brendon5sec">brendon5sec - ssr with 5 sec cache age</a><br />
     <a href="/nitro">nitro - static with subpage</a>
   </p>
 </div>`);
